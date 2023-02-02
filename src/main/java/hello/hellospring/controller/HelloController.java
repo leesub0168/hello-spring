b59c1e26ba7ddc1ba5d84c2e42a113c0bed3c6fd
leeso <leesub0168@gmail.com>
2023-02-02 21:40:37 +0900

    스프링 테스트 코드

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -1,15 +1,48 @@
 package hello.hellospring.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
 
+    private Logger logger = LoggerFactory.getLogger(HelloController.class);
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(value = "name", required = false) String name, Model model) {
+        model.addAttribute("name",name);
+        return "hello-template";
+    }
+
     @GetMapping("hello")
     public String hello(Model model) {
-        model.addAttribute("data", "spring!!");
+        model.addAttribute("data", "spring!!???");
+        logger.info("hello >>>>>>>>>>>>>>>");
         return "hello";
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
